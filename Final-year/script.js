5fe2e93387cec22cd827a5e098b05eaa25dd0535
ABDUL HAJEES <84639525+aj05hacker@users.noreply.github.com>
2023-05-12 22:03:32 +0530

    comeback

--- Final-year/script.js
+++ Final-year/script.js
@@ -1,1 +1,9 @@
-// 404
+document.addEventListener("visibilitychange", function() {
+    if (document.visibilityState === 'hidden') {
+      // Change the tab title when the page is hidden
+      document.title = "Come Back!";
+    } else {
+      // Restore the original tab title when the page is visible again
+      document.title = "Miss you all";
+    }
+  });
